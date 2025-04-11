739a8c065e63400c89faf88d699f74ec0164b501
Undeemiss <jade@hewittfamily.com>
2025-04-11 14:41:22 -0500

    Rework purchaseServer.ts for price estimates as well as purchasing

--- servers/home/earlyGame/purchaseServer.ts
+++ servers/home/earlyGame/purchaseServer.ts
@@ -2,6 +2,12 @@ import { getHosts } from "../utils/getHosts";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    const size = <number> eval(<string> ns.args[1])
-    ns.tprint(ns.purchaseServer(<string>ns.args[0], size));
+    const size = <number>eval(<string>ns.args[0])
+    // Print the cost
+    ns.tprint(ns.formatNumber(ns.getPurchasedServerCost(size)));
+
+    // If a name was given, but the server
+    if (ns.args[1]) {
+        ns.tprint(ns.purchaseServer(<string>ns.args[1], size));
+    }
 }
