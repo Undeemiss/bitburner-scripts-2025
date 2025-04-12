620a90887f56448b0ed0a14dc9373d670a50a7d7
Undeemiss <jade@hewittfamily.com>
2025-04-11 20:02:54 -0500

    First complete version of batcher (still untested)

--- servers/home/batching/shotgun.ts
+++ servers/home/batching/shotgun.ts
@@ -11,11 +11,70 @@ export async function main(ns: NS) {
     let target = ns.getServer(<string>ns.args[0]);
     let botnet = getHosts(ns);
 
-    // TODO
+    // Start Batching
     await soften(ns, target.hostname, botnet);
-    // Start Batching. This can be done immediately, without waiting,
-    // if there is RAM left over after assigning the final softening threads.
+    await batch(ns, target.hostname, botnet);
+}
 
+
+async function batch(ns: NS, targetHostname: string, botnet: Set<string>) {
+    // Initialize variables
+    const weakenEffectiveness = ns.weakenAnalyze(1);
+    const weakensPerHack = balanceRatio(weakenEffectiveness);
+    const hackEfficiencyFactor = (1 / (1 + weakensPerHack));
+    const weakensPerGrow = 2 * balanceRatio(weakenEffectiveness);
+    const growEfficiencyFactor = (1 / (1 + weakensPerGrow));
+
+    while (true) {
+        // Get basic server information
+        const target = ns.getServer(targetHostname);
+        const player = ns.getPlayer();
+        const projectedTarget = target;
+
+        const threadsAvailable = getMaxThreads(ns, botnet, 1.75);
+
+        // Calculate how many hack threads are necessary to drain the target completely
+        projectedTarget.hackDifficulty = target.minDifficulty; // This affects hack/grow amount at completion
+        // TODO: Account for hacking skill level ups
+        const threadHackPercent = ns.formulas.hacking.hackPercent(projectedTarget, player)
+        const absMaxHackThreads = Math.ceil(1 / threadHackPercent);
+
+        const hackThreads = absMaxHackThreads;
+        projectedTarget.moneyAvailable = Math.max(0, target.moneyMax * (hackThreads * threadHackPercent));
+        const growThreads = ns.formulas.hacking.growThreads(projectedTarget, player, target.moneyMax);
+        const hackWeakens = Math.ceil(hackThreads * weakensPerHack);
+        const growWeakens = Math.ceil(growThreads * weakensPerGrow);
+        const threadsPerBatch = hackThreads + hackWeakens + growThreads + growWeakens;
+
+        const batches = Math.floor(threadsAvailable / threadsPerBatch);
+
+        const weakenTime = ns.formulas.hacking.weakenTime(target, player);
+        const hackDeltaTime = weakenTime - ns.formulas.hacking.hackTime(target, player);
+        const growDeltaTime = weakenTime - ns.formulas.hacking.growTime(target, player);
+
+        for (let i = 0; i < batches; i++) {
+            let failed = 0;
+            failed += execOnBotnet(ns, botnet, 'batching/hack.js', hackThreads, [targetHostname, hackDeltaTime]);
+            failed += execOnBotnet(ns, botnet, 'batching/weaken.js', hackWeakens, [targetHostname]);
+            failed += execOnBotnet(ns, botnet, 'batching/grow.js', growThreads, [targetHostname, growDeltaTime]);
+            failed += execOnBotnet(ns, botnet, 'batching/weaken.js', hackWeakens, [targetHostname]);
+            if (failed != 0) {
+                throw Error(`Failed to allocate ${failed} requested batching threads.`);
+            }
+        }
+
+        if (batches > 0) {
+            await ns.sleep(sleepBuffer + weakenTime);
+        } else {
+            await ns.sleep(wakeupTimer);
+        }
+    }
+}
+
+function assumeSoftened(server: Server) {
+    const newServer = server;
+    newServer.hackDifficulty = server.minDifficulty;
+    newServer.moneyAvailable = server.moneyMax;
 }
 
 // Prepares a server for batching. Returns once the last operation is started,
@@ -27,13 +86,17 @@ async function soften(ns: NS, targetHostname: string, botnet: Set<string>) {
 
 async function initialWeaken(ns: NS, targetHostname: string, botnet: Set<string>) {
     // Initialize variables
-    const target = ns.getServer(targetHostname);
-    const player = ns.getPlayer();
+    let target = ns.getServer(targetHostname);
+    let player = ns.getPlayer();
     const weakenEffectiveness = ns.weakenAnalyze(1);
 
     // Weaken until the security is minimized
     let weakenThreadsRemaining = Math.ceil((target.hackDifficulty - target.minDifficulty) / weakenEffectiveness);
     while (weakenThreadsRemaining > 0) {
+        // Ensure accurate data for formulas
+        target = ns.getServer(targetHostname);
+        player = ns.getPlayer();
+
         const returned = execOnBotnet(ns, botnet, 'batching/weaken.js', weakenThreadsRemaining, [targetHostname]);
 
         // Sleep if more weakening is required
@@ -50,8 +113,8 @@ async function initialWeaken(ns: NS, targetHostname: string, botnet: Set<string>
 
 async function initialGrow(ns: NS, targetHostname: string, botnet: Set<string>) {
     // Initialize variables
-    const target = ns.getServer(targetHostname);
-    const player = ns.getPlayer();
+    let target = ns.getServer(targetHostname);
+    let player = ns.getPlayer();
     const weakenEffectiveness = ns.weakenAnalyze(1);
     const weakensPerGrow = 2 * balanceRatio(weakenEffectiveness);
     const efficiencyFactor = (1 / (1 + weakensPerGrow));
@@ -59,6 +122,10 @@ async function initialGrow(ns: NS, targetHostname: string, botnet: Set<string>) 
     // Grow until the server is completely full
     let growThreadsRemaining = ns.formulas.hacking.growThreads(target, player, target.moneyMax);
     while (growThreadsRemaining > 0) {
+        // Ensure accurate data for formulas
+        target = ns.getServer(targetHostname);
+        player = ns.getPlayer();
+
         // Calculate how many grow/weaken threads to use with available RAM
         const availableThreads = getMaxThreads(ns, botnet, 1.75);
         const growThreads = Math.min(Math.floor(availableThreads * efficiencyFactor), growThreadsRemaining);
@@ -87,25 +154,8 @@ async function initialGrow(ns: NS, targetHostname: string, botnet: Set<string>) 
     }
 }
 
-// Get the number of weaken threads required to minimize server security.
-function weakenThreads(target: Server, weakenEffectiveness: number) {
-    return Math.ceil(target.hackDifficulty - target.minDifficulty / weakenEffectiveness);
-}
-
-// Get the number of weaken threads required to offset a number of grow or hack threads.
-// Combined simply because this offers greater flexibility.
-function balanceThreads(growThreads: number, hackThreads: number, weakenEffectiveness: number) {
-    return Math.ceil(((growThreads * 0.004) + (hackThreads * 0.002) / weakenEffectiveness))
-}
-
 function balanceRatio(weakenEffectiveness: number) {
     return (0.002 / weakenEffectiveness)
-}
-function growBalance(growThreads: number, weakenEffectiveness: number) {
-    return Math.ceil(growThreads * 2 * balanceRatio(weakenEffectiveness))
-}
-function hackBalance(hackThreads: number, weakenEffectiveness: number) {
-    return Math.ceil(hackThreads * balanceRatio(weakenEffectiveness))
 }
 
 export function autocomplete(data: AutocompleteData) {
