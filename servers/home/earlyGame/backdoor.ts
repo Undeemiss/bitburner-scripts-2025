9adacf37da7fa75962af04a9adf24422efbb84e2
Undeemiss <jade@hewittfamily.com>
2025-04-12 15:36:46 -0500

    Backdoor script prioritizes important targets

--- servers/home/earlyGame/backdoor.ts
+++ servers/home/earlyGame/backdoor.ts
@@ -6,28 +6,41 @@ export async function main(ns: NS) {
     ns.enableLog('singularity.installBackdoor');
     ns.ui.openTail();
 
-    const hosts = getHosts(ns);
+    const priorityHosts = new Set<string>(['CSEC', 'avmnite-02h', 'I.I.I.I', 'run4theh111z']);
+    for (const hostname of priorityHosts) {
+        await backdoor(ns, hostname);
+    }
 
+    ns.tprint('Priority Hosts Done');
+    ns.print('Priority Hosts Done');
+
+    const hosts = getHosts(ns);
     for (const hostname of hosts) {
-        farConnect(ns, hostname);
-        if (!ns.getServer(hostname).backdoorInstalled) {
+        await backdoor(ns, hostname);
+    }
+
+    ns.singularity.connect('home');
+
+    ns.tprint('Done');
+    ns.print('Done');
+}
+
+async function backdoor(ns: NS, hostname: string) {
+    farConnect(ns, hostname);
+    if (!ns.getServer(hostname).backdoorInstalled) {
+        try {
+            await ns.singularity.installBackdoor();
+            ns.tprint(`Backdoored '${hostname}'`);
+            ns.print(`Backdoored '${hostname}'`);
+        } catch (e) { // Stupid error handling
             try {
-                await ns.singularity.installBackdoor();
-                ns.tprint(`Backdoored '${hostname}'`);
-                ns.print(`Backdoored '${hostname}'`);
-            } catch (e) { // Stupid error handling
-                try {
-                    if (!(e.includes('singularity.installBackdoor: Cannot '))) {
-                        throw e;
-                    }
-                    // ns.tprint(`Skipping '${hostname}'`);
-                } catch (_) {
+                if (!(e.includes('singularity.installBackdoor: Cannot '))) {
                     throw e;
                 }
+                // ns.tprint(`Skipping '${hostname}'`);
+            } catch (_) {
+                throw e;
             }
         }
     }
-    ns.singularity.connect('home');
-    ns.tprint('Done');
-    ns.print('Done');
 }
