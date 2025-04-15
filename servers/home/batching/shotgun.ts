4925778e0a7496e13e9a166d4d183e0d9d083dff
Undeemiss <jade@hewittfamily.com>
2025-04-14 22:47:12 -0500

    lower the hardcoded batching value... this needs improvement later, once I can handle more then literally 8 batches at a time again.

--- servers/home/batching/shotgun.ts
+++ servers/home/batching/shotgun.ts
@@ -39,7 +39,7 @@ async function batch(ns: NS, targetHostname: string, botnet: Set<string>) {
         const threadHackPercent = ns.formulas.hacking.hackPercent(projectedTarget, player)
         const absMaxHackThreads = Math.ceil(1 / threadHackPercent);
 
-        const hackThreads = absMaxHackThreads;
+        const hackThreads = Math.ceil(absMaxHackThreads * 0.01);
         projectedTarget.moneyAvailable = Math.max(0, target.moneyMax * (1 - (hackThreads * threadHackPercent)));
         const growThreads = ns.formulas.hacking.growThreads(projectedTarget, player, target.moneyMax);
         const hackWeakens = Math.ceil(hackThreads * weakensPerHack);
