92e825f65c4de0fbcd525e18fa37227655b34f3f
Undeemiss <jade@hewittfamily.com>
2025-04-27 23:01:48 -0500

    persistent karma view mode

--- servers/home/utils/karma.ts
+++ servers/home/utils/karma.ts
@@ -1,6 +1,21 @@
+import { AutocompleteData } from "@/NetscriptDefinitions";
+
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    const karma = ns.getPlayer().karma;
+    let karma = ns.getPlayer().karma;
     const gangRatio = karma / -54000;
     ns.tprint(`${karma} (${ns.formatPercent(gangRatio)} of gang requirements)`);
+
+    if (ns.args[0] == 'persist') {
+        ns.ui.openTail();
+        while (true) {
+            karma = ns.getPlayer().karma;
+            ns.print(`${karma} (${ns.formatPercent(gangRatio)} of gang requirements)`);
+            await ns.sleep(10000);
+        }
+    }
+}
+
+export function autocomplete(data: AutocompleteData) {
+    return ['persist', '--tail'];
 }
